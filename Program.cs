d9490ce9fd10d0b5ec47901d59a5fa0449f0c0ef
Elcana <mvbelcan@gmail.com>
2020-08-09 14:17:13 -0300

    inicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projeto__teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
