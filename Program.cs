8d6a6c2cd2ed01f6b7f2379d7e90b9dec276ac40
Elcana <mvbelcan@gmail.com>
2020-08-09 14:30:00 -0300

    Acrescentada palavra bodia

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ namespace projeto__teste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("bodia")
     }
 }
